e73eb43c689ded858edd0c36f7d17bd1dd21bf21
brycycling <chowbry@gmail.com>
2025-05-26 14:32:42 -0700

    first draft

---
+++ categorizer.js
@@ -0,0 +1,221 @@
+const PSYCHIATRIC_DIAGNOSES = [
+    "schizophrenia",
+    "schizoaffective",
+    "delusion",
+    "bipolar",
+    "psychotic",
+    "psychosis",
+    "OCD",
+    "obsessive",
+    "compulsive",
+    "hallucination",
+]
+
+const NEUROCOGNITIVE_DIAGNOSES = [
+    "dementia",
+    "alzheimers",
+    "parkinsons",
+    "lewy",
+    "frontotemporal",
+    "ftd",
+    "ftld",
+    "vascular",
+    "ncd",
+    "bpsd",
+    "neurocognitive",
+]
+
+const OTHER_DIAGNOSES = [
+    "huntington",
+    "huntingtons"
+]
+
+const END_OF_LIFE_DIAGNOSES = [
+    "palliative",
+    "hospice",
+    "terminal",
+]
+const REGULAR_ANTIPSYCHOTICS = [
+    "haloperidol",
+    "haldol",
+    "risperidone",
+    "risperdal",
+    "olanzapine",
+    "zyprexa",
+    "quetiapine",
+    "seroquel",
+    "aripiprazole",
+    "abilify",
+    "loxapine",
+    "methotrimeprazine",
+]
+
+const SPECIAL_ANTIPSYCHOTICS = [
+    "clozapine",
+    "clozaril",
+    "amisulpride",
+    "sulpiride",
+    "asenapine",
+    "saphris",
+    "iloperidone",
+    "fanapt",
+    "lurasidone",
+    "latuda",
+    "paliperidone",
+    "invega",
+    "ziprasidone",
+    "geodon",
+    "cariprazine",
+    "vraylar",
+    "brexpiprazole",
+    "rexulti",
+    "cariprazine",
+    "vraylar",
+    "pimavanserin",
+    "nuplazid",
+    "flupentixol",
+    "zuclopenthixol",
+    "fluphenazine",
+    "prochlorperazine",
+    "perphenazine",
+    "thiothixene",
+    "trifluoperazine",
+    "chlorpromazine",
+    "Fluanxol",
+    "Clopixol",
+    "Stelazine",
+    "zotepine",
+    "depot",
+    "long acting",
+    "injectable",
+    "lai",
+]
+
+const BEHAVIOR_RESPONSIVE = [
+    "delirium",
+    "delusions",
+    "paranoia",
+    "auditory_hallucinations",
+    "visual_hallucinations",
+    "resistance_to_care",
+    "sexually_inappropriate_behavior",
+    "physical_aggression",
+    "verbal_aggression",
+];
+
+const BEHAVIOR_NON_RESPONSIVE = [
+    "depression",
+    "low_mood",
+    "anxiety",
+    "irritability",
+    "crying",
+    "apathy",
+    "calling_out",
+    "repetitive_activities",
+    "hoarding",
+    "incontinence",
+    "inappropriate_undressing",
+    "inappropriate_defecation",
+    "inappropriate_urination",
+    "wandering",
+    "exit_seeking",
+    "sundowning",
+    "insomnia",
+    "climbing_out",
+    "spitting",
+];
+
+var isPsychiatricDx = false
+var isNeurocognitiveDx = false
+var isOtherDx = false
+var isEndOfLife = false
+var isMHTInvolved = false
+
+var isSpecialAntipsychotic = false
+var isAnyAntipsychotic = false
+
+var isBehaviorPresent = false
+var isBehaviorAPResponsive = false
+var isBehaviorOngoing = false
+var isBehaviorStableLess3Months = false
+var isBehaviorStableMore3Months = false
+
+var isNonpharmTried = false
+var isSideEffect = false
+
+// ask user to input a list of diagnoses in terminal using readline
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+async function categorize() {
+    const diagnoses = await new Promise((resolve) => {
+        rl.question("Enter a list of diagnoses (comma separated): ", (input) => {
+            resolve(input.split(',').map(d => d.trim().toLowerCase()));
+        });
+    });
+    //remove punctuation from diagnoses
+    const cleanedDiagnoses = diagnoses.map(d => d.replace(/[.,!?;:']/g, ''));
+
+    // categorize diagnoses, allowing for fuzzy matching
+    cleanedDiagnoses.forEach(diagnosis => {
+        if (PSYCHIATRIC_DIAGNOSES.some(d => d.includes(diagnosis))) {
+            isPsychiatricDx = true;
+        } else if (NEUROCOGNITIVE_DIAGNOSES.some(d => d.includes(diagnosis))) {
+            isNeurocognitiveDx = true;
+        } else if (OTHER_DIAGNOSES.some(d => d.includes(diagnosis))) {
+            isOtherDx = true;
+        } else if (END_OF_LIFE_DIAGNOSES.some(d => d.includes(diagnosis))) {
+            isEndOfLife = true;
+        }
+    });
+
+    // ask user to input list of medications in terminal
+    const medications = await new Promise((resolve) => {
+        rl.question("Enter a list of medications (comma separated): ", (input) => {
+            resolve(input.split(',').map(m => m.trim().toLowerCase()));
+        });
+    });
+
+    // identify if medications inlude special antipsychotics or regular antipsychotics, then check if special antipsyhotic, if not then note that they are any antipsychotic
+    medications.forEach(medication => {
+        if (SPECIAL_ANTIPSYCHOTICS.some(m => m.includes(medication))) {
+            isSpecialAntipsychotic = true;
+        } else if (REGULAR_ANTIPSYCHOTICS.some(m => m.includes(medication))) {
+            isAnyAntipsychotic = true;
+        }
+    });
+
+
+
+
+    printCategorizationResults();
+
+}    
+
+
+    // print values of each variable
+function printCategorizationResults() {
+    console.log("Psychiatric Diagnosis:", isPsychiatricDx);
+    console.log("Neurocognitive Diagnosis:", isNeurocognitiveDx);
+    console.log("Other Diagnosis:", isOtherDx);
+    console.log("End of Life Diagnosis:", isEndOfLife);
+    console.log("");
+    console.log("MHT Involved:", isMHTInvolved);
+    console.log("Special Antipsychotic:", isSpecialAntipsychotic);
+    console.log("Any Antipsychotic:", isAnyAntipsychotic);
+    console.log("Behavior Present:", isBehaviorPresent);
+    console.log("Behavior AP Responsive:", isBehaviorAPResponsive);
+    console.log("Behavior Ongoing:", isBehaviorOngoing);
+    console.log("Behavior Stable Less than 3 Months:", isBehaviorStableLess3Months);
+    console.log("Behavior Stable More than 3 Months:", isBehaviorStableMore3Months);
+    console.log("Non-pharmacological Treatment Tried:", isNonpharmTried);
+    console.log("Side Effect Present:", isSideEffect);
+
+    rl.close();
+}
+categorize();
+
